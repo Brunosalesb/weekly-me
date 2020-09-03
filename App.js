4b64085f69c852d581b3c3a1947d5f6d6881c795
Bruno Sales <bruno.sbernardes01@gmail.com>
2020-09-03 19:04:21 -0300

    Firebase getAllDocs

--- App.js
+++ App.js
@@ -4,22 +4,19 @@ import firestore, { firebase } from "@react-native-firebase/firestore";
 
 class App extends Component {
   state = {
-    activities: {
-      description: "",
-      hour: ""
-    }
+    activities: []
   }
-  constructor(props){
+
+  constructor(props) {
     super(props);
-      // this.getActivity();
-      this.subscriber = firestore().collection("activities").doc("ETSwxGlBB2ogAkK4kRPv").onSnapshot(doc => {
-        this.setState({
-          activities: {
-            description: doc.data().description,
-            hour: doc.data().hour
-          }
-        })
-      }) 
+    // this.getActivity();
+    this.subscriber = firestore().collection("activities").onSnapshot(docs => {
+      let activities = []
+      docs.forEach(doc => {
+        activities.push(doc.data())
+      })
+      this.setState({ activities })
+    })
   }
 
   getActivity = async () => {
@@ -30,7 +27,12 @@ class App extends Component {
   render() {
     return (
       <View>
-        <Text>Atividade: {this.state.activities.description}</Text>
+        {this.state.activities.map((activity, index) =>
+          <View key={index}>
+            <Text>Atividade - {activity.description}</Text>
+          </View>
+        )}
+        {/* <Text>Atividade: {this.state.activities.description}</Text> */}
       </View >
     );
   };
